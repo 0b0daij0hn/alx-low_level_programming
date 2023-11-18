60f012aa16a4a2002e5dcc5b86b28d50d8be4b32
johnobodai <j.obodai@alustudent.com>
2023-11-18 15:39:57 +0000

    correct to suit betty standards

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-	if (argc > 0) {
-		printf("%s\n", argv[0]);
-		return 0;
-	} else {
-		return 1;
-	}
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+    if (argc > 0)
+    {
+        printf("%s\n", argv[0]);
+        return (0);
+    }
+    else
+    {
+        return (1); /* Indicates an error */
+    }
 }
+
